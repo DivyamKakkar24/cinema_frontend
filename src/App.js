0d09db21a195a30f09b44a84c95f6a8b8368360b
DivyamKakkar24 <kakkardivyam24@gmail.com>
2023-05-24 14:50:57 +0530

    reloading fixed 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
 import Layout from "./components/Layout";
-import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
@@ -13,19 +13,17 @@ import Construction from "./components/construction/Construction";
 function App() {
 
   return (
-    <Router basename="/">
-        <div className="App">
-            <Header />
-            <Routes>
-                <Route path = "/" element = {<Layout/>}>
-                    <Route path = "/" element = {<Home />} />
-                    <Route path = "/Trailer/:ytTrailerId" element = {<Trailer />} />
-                    <Route path = "/Reviews/:movieId" element = {<PublicOpinion />} />
-                    <Route path = "*" element = {<Construction />} />
-                </Route>
-            </Routes>
-        </div>
-    </Router>
+    <div className="App">
+        <Header />
+        <Routes>
+            <Route path = "/" element = {<Layout/>}>
+                <Route path = "/" element = {<Home />} />
+                <Route path = "/Trailer/:ytTrailerId" element = {<Trailer />} />
+                <Route path = "/Reviews/:movieId" element = {<PublicOpinion />} />
+                <Route path = "*" element = {<Construction />} />
+            </Route>
+        </Routes>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  	<BrowserRouter>
+  	<BrowserRouter basename="/">
   		<Routes>
   			<Route path = "/*" element = {<App />} />
   		</Routes>
