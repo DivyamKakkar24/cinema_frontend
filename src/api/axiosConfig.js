fff2b871198355709a233082475098e0e11ab8b1
DivyamKakkar24 <kakkardivyam24@gmail.com>
2023-09-16 16:48:19 +0530

    fixed delay

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -4,3 +4,5 @@ export default axios.create({
 	baseURL:'https://cinema-production.up.railway.app',
     headers: {"ngrok-skip-browser-warning": "true","Access-Control-Allow-Origin": "*"}
 });
+
+// previous api at: https://spotlight-cinema-app.onrender.com
