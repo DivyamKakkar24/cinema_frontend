cf8588aee0c63b9cd2993911299797179637d670
DivyamKakkar24 <kakkardivyam24@gmail.com>
2023-11-07 21:14:05 +0530

    service changed

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL:'https://cinema-production.up.railway.app',
+	baseURL:'https://spotlight-cinema-app.onrender.com',
     headers: {"ngrok-skip-browser-warning": "true","Access-Control-Allow-Origin": "*"}
 });
 
-// previous api at: https://spotlight-cinema-app.onrender.com
+
+// previous api at: https://cinema-production.up.railway.app
